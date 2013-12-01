4390d897d21426c5e53a79439fe600cf53acc58a
Alexandertyler <Alexanderhtyler@gmail.com>
2013-11-30 18:39:17 -0600

    Initialize repo

---
+++ clothsim.cpp
@@ -0,0 +1,63 @@
+#include "clothsim.h"
+/*NOTES FOR OTHER TEAM MEMBERS
+	1. Need to move shit to a header.
+
+*/
+
+/*GLOBAL VARIABLES*/
+vector<Particle> particleVector;
+
+Particle()
+{
+	return;
+}
+
+ParticleSystem()
+{
+	return;
+}
+
+//we should do an evalForce function in each class for the different objects. Makes it 
+//more specialized that way.
+
+
+
+
+//evaluate the force acting on each particle
+void evalForce(){
+	//pseudocode:
+	
+	
+
+	//foreach particle, add the gravity force
+	//foreach triangle in the cloth plane, calculate and add the wind
+	//foreach spring, calculate and add in spring force to particles
+
+}
+
+class Sphere
+{
+
+};
+/* NOTES/THINGS TO CONSIDER
+
+- for the particle numerical integration, we were thinking about doing either Euler or Verlet,
+though verlet seems more desirable.
+- should we be thinking about the optimal number of particles?  -> right now,
+we're thinking about going with having a system that can work with an number of particles
+(will use an integer variable, numParticles)
+
+
+*/
+
+void initializeCloth() 
+{
+	for (int i=0; i<numParticles; i++)
+	{
+		//initialize a new particle and add it to the vector
+		Particle currParticle();
+		particleVector.push_back(currParticle);
+		
+		
+
+}
---
+++ clothsim.h
@@ -0,0 +1,68 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/time.h>
+
+#ifdef OSX
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/glut.h>
+#include <GL/glu.h>
+#endif
+
+#include <stdexcept>
+#include <time.h>
+#include <math.h>
+
+/** Particle class definition. Defines a particle with a mass, position, and velocity attributes. */
+class Particle {
+public:
+	//Particle constructor:
+  Particle(float _mass, vector<float> _pos, vector<float> _velocity) {
+  	mass = _mass;
+  	pos = _pos;
+  	vel = _velocity; //velocity will not be needed if we go with Verlet integration
+  }
+
+
+private:
+	vector<Particle> linkedParticles;
+	bool canMove;
+	float mass;
+	vector<float>  pos, oldPos, vel;
+};
+
+/** Particle system, sets up system of particle linking. */
+//cloth
+
+class ParticleSystem {
+public:
+	ParticleSystem();
+	
+	//we should add the particles into a vector when we initialize them. Because
+	//we only have one object utilizing this vector, it shouldn't be a problem.
+	//somehow, set up linking system: iterate through all particles, and link them.
+	//THINK ABOUT HOW TO DO LINKING SYSTEM???
+private:
+	int numParticles;
+};
+
+
+class Sphere
+{
+
+};
+
+
+//Functions
+
+void evalForce;
+void initializeCloth;
+
